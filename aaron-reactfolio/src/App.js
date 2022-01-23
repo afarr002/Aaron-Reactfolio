54d0b46d52366f7668f93981a03345e7b16afb48
aaron <afarrell002@gmail.com>
2022-01-23 16:35:13 -0700

    imported More.js to App.js and added More JSX to return

--- aaron-reactfolio/src/App.js
+++ aaron-reactfolio/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import More from "./components/More";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 
-export default function App() {
+const App = () => {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
+      <More />
       <Projects />
       <Skills />
       <Testimonials />
@@ -19,4 +21,6 @@ export default function App() {
       <Footer />
     </main>
   );
-}
+};
+
+export default App;
