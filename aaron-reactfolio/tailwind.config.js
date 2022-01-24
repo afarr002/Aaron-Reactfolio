972e61a9a40907d35aad39e1b11ee03cf420b3b6
aaron <afarrell002@gmail.com>
2022-01-23 19:17:17 -0700

    removed dark thene key, updated customer colors for tailwinds.config.js

--- aaron-reactfolio/tailwind.config.js
+++ aaron-reactfolio/tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkorange: "rgb(234 88 12)",
+        lightorange: "rgb(249 115 22)",
+        darkneutral: "rgb(115 115 115)",
+        lightneutral: "rgb(212 212 212)",
+        black: "rgb(0 0 0)",
+      },
+    },
   },
   variants: {
     extend: {},
