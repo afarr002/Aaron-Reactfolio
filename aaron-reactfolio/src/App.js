1219b4e2a899d5aacaa0e2b028c9fab3ce401a7c
aaron <afarrell002@gmail.com>
2022-01-23 17:06:00 -0700

    used react-router-dom help routing the app through the navbar

--- aaron-reactfolio/src/App.js
+++ aaron-reactfolio/src/App.js
@@ -7,19 +7,26 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <Router basename="/">
       <Navbar />
-      <About />
-      <More />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
+
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/more" component={More} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/skills" component={Skills} />
+          <Route exact path="/testimonials" component={Testimonials} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </main>
+
       <Footer />
-    </main>
+    </Router>
   );
 };
 
