593f119948e1bddbfbfbfdd19116aa9cc56a8fb0
aaron <afarrell002@gmail.com>
2022-01-23 19:10:52 -0700

    changed css styling for application consistency - Navbar.js

--- aaron-reactfolio/src/components/Navbar.js
+++ aaron-reactfolio/src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+        <a className="title-font font-medium text-darkneutral hover:text-lightneutral mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Aaron
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-lightneutral justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Portfolio
           </a>
@@ -23,7 +23,7 @@ const Navbar = () => {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center border-0 py-1 px-3 text-darkneutral hover:bg-darkorange rounded hover:text-lightneutral mt-4 md:mt-0"
         >
           Contact Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
