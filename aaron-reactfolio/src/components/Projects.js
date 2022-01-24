d9b943beef833ef099c7bf47533bed805301b656
aaron <afarrell002@gmail.com>
2022-01-23 19:09:29 -0700

    changed media responsiveness, added button for link to github repo

--- aaron-reactfolio/src/components/Projects.js
+++ aaron-reactfolio/src/components/Projects.js
@@ -5,13 +5,13 @@ import { portfolio } from "../seed/portfolio";
 const Projects = () => {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container px-5 py-10 mx-auto text-center">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-darkneutral">
             My Work!
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lightneutral">
             Here is a sample of some of my favorite projects that I've built so
             far! I'm proud to note that while (currently) everything is
             JavaScript based, I worked hard to learn different frameworks and
@@ -21,22 +21,34 @@ const Projects = () => {
         </div>
         <div className="flex flex-wrap -m-4">
           {portfolio.map(({ link, image, title, description, gitlink }) => (
-            <a href={link} key={image} className="sm:w-1/2 w-100 p-4">
+            <a
+              href={link}
+              key={image}
+              target="_blank"
+              className="sm:w-1/2 w-100 p-4"
+            >
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-contain object-center"
                   src={image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-lg font-medium text-darkneutral mb-3">
                     {title}
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed text-lightneutral">
                     {description}
-                    <br />
-                    Github Repo: {gitlink}
                   </p>
+                  <br />
+                  <a
+                    href={gitlink}
+                    key={image}
+                    target="_blank"
+                    className="p-4 rounded hover:bg-darkorange hover:text-lightneutral"
+                  >
+                    Click here to visit GitHub Repo!
+                  </a>
                 </div>
               </div>
             </a>
