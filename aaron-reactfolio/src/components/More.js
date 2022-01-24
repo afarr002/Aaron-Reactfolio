206a1f55ccc3cc597a0b34306924d2cbe9106789
aaron <afarrell002@gmail.com>
2022-01-23 19:11:41 -0700

    changed css styling for application consistency - More.js

--- aaron-reactfolio/src/components/More.js
+++ aaron-reactfolio/src/components/More.js
@@ -5,10 +5,6 @@ const More = () => {
     <section id="more">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Aaron!
-          </h1>
-          <br />
           <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Where I'm From!
           </h2>
@@ -21,27 +17,23 @@ const More = () => {
             Some of my favorite hobbies!
           </h2>
           <p className="mb-8 leading-relaxed">
-            <ul className="list-none md:list-inside md:hover:list-outside md:hover:list-disc">
-              <li>Coding! I've somehow fallen in love with functions.</li>
-              <li>
-                Spending time with my dog Margot. (She approves all my code
-                before it goes to production)
-              </li>
-              <li>Listening to music (probably way too loud).</li>
-              <li>Rooting for the Broncos (certianly way too loud).</li>
-              <li>
-                Spending time outside, be it hiking, or sports (disc golf is my
-                current hobby of obsession), or a BBQ with friends.
-              </li>
-              <li>
-                Reading all kind of books. I enjoy fiction and non fiction, and
-                I especially enjoy horror novels and military history.
-              </li>
-              <li>
-                Learning new things and using that knowledge to solve life's
-                problems, little or big!
-              </li>
-            </ul>
+            Coding! I've somehow fallen in love with functions.
+            <br />
+            Spending time with my dog Margot. (She approves all my code before
+            it goes to production)
+            <br />
+            Listening to music (probably way too loud).
+            <br />
+            Rooting for the Broncos (certianly way too loud).
+            <br />
+            Spending time outside, be it hiking, or sports (disc golf is my
+            current hobby of obsession), or a BBQ with friends.
+            <br />
+            Reading all kind of books. I enjoy fiction and non fiction, and I
+            especially enjoy horror novels and military history.
+            <br />
+            Learning new things and using that knowledge to solve life's
+            problems, little or big!
           </p>
           <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             What did I learn during quarentine?
@@ -61,21 +53,21 @@ const More = () => {
           <div className="flex justify-center">
             <a
               href="#about"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="inline-flex text-white bg-darkorange border-0 py-2 px-6 focus:outline-none hover:bg-lightorange rounded text-lg"
             >
-              About face! (Return to previous About page)
+              Back to About
             </a>
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="ml-4 inline-flex text-white bg-darkorange border-0 py-2 px-6 focus:outline-none hover:bg-lightorange rounded text-lg"
             >
               Work With Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              className="ml-4 inline-flex text-white bg-darkorange border-0 py-2 px-6 focus:outline-none hover:bg-lightorange rounded text-lg"
             >
-              See My Past Work
+              See My Portfolio
             </a>
           </div>
         </div>
