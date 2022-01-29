90e74171819b1affa248a81ad27e3f24442f96dd
aaron <afarrell002@gmail.com>
2022-01-29 10:16:30 -0700

    updated resume and contact me icons in navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRightIcon, DownloadIcon } from "@heroicons/react/solid";
+import { AtSymbolIcon, DocumentDownloadIcon } from "@heroicons/react/solid";
 
 const Navbar = () => {
   return (
@@ -28,13 +28,13 @@ const Navbar = () => {
           className="inline-flex items-center md:border-r md:border-gray-700 py-1 px-3 text-darkneutral hover:bg-gray-900 hover:border-gray-900 hover:text-darkorange hover:rounded mt-4 md:mt-0"
         >
           Resume
-          <DownloadIcon className="w-4 h-4 ml-1 text-darkorange" />
+          <DocumentDownloadIcon className="w-4 h-4 ml-1 text-darkorange" />
         </a>
         <a
           href="#contact"
           className="inline-flex items-center md:border-l md:border-gray-700 py-1 px-3 text-darkneutral hover:bg-gray-900 hover:rounded hover:border-gray-900 hover:text-darkorange mt-4 md:mt-0"
         >
-          Contact Me <ArrowRightIcon className="w-4 h-4 ml-1 text-darkorange" />
+          Contact Me <AtSymbolIcon className="w-4 h-4 ml-1 text-darkorange" />
         </a>
       </div>
     </header>
